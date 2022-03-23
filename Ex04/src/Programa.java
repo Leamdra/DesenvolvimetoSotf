9d6ada6896613d555a0e903820bbc3ac9a6f011c
Leamdra <100883313+Leamdra@users.noreply.github.com>
2022-03-22 21:25:54 -0300

    Add files via upload

--- Ex04/src/Programa.java
+++ Ex04/src/Programa.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class programa {
+public class Programa {
 	
 public static void main (String[] args) {
 	
@@ -15,17 +15,26 @@ public static void main (String[] args) {
 	
 	double[]notas = new double [5];
 	
+	
+	double[] pesonotas = new double[5];
+	pesonotas [0] = 0.05;
+	pesonotas [1] = 0.05;
+	pesonotas [2] = 0.2 ;
+	pesonotas [3] = 0.2 ;
+	pesonotas [4] = 0.5 ;
+	
+	
 	int contador = 0;
 	do { 
 		System.out.println("Informar uma nota entre 0 e 10.");
 		System.out.println("Digite a " + (contador + 1) + "a nota: ");
 		double temp = leitor.nextDouble();
 		
-		if (temp >= 0 && temp < 10) {
-			
-		contador++;
-		} else { 
+		if (temp < 0 && temp > 10) {
 			System.out.println("A nota " + temp + "é invalida");
+		} else { 
+			notas [contador] = temp;
+			contador++;		
 		}
 		
 	} while(contador < 5);
@@ -39,6 +48,25 @@ public static void main (String[] args) {
 		System.out.println("Nota: " + (i + 1) + ":" + notas[i]);
 	}
 	
+	double[] calculo = new double [5];
+	calculo [0] = (notas[0] * pesonotas [0]);
+	calculo [1] = (notas[1] * pesonotas [1]);
+	calculo [2] = (notas[2] * pesonotas [2]);
+	calculo [3]	= (notas[3] * pesonotas [3]);	
+	calculo [4] = (notas[4] * pesonotas [4]);
+	
+	double nFinal;
+	nFinal = (calculo[0] + calculo[1] + calculo[2] +calculo[3] + calculo [4]);	
+	System.out.println("Sua nota final é :" + nFinal);
+			
+	if (nFinal >= 6)   {
+		
+		System.out.println("Aprovado: (x)Sim ( )Não");
+		
+	} else {
+		System.out.println("Aprovado: ( )Sim (x)Não");
+	}
+
 	leitor.close();
 	
 	}
