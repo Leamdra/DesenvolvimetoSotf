dd43c70dc85580d8763d309e23c138c1214dd625
Leamdra <100883313+Leamdra@users.noreply.github.com>
2022-04-05 22:03:41 -0300

    Add files via upload

---
+++ Aula0504/src/grupo.java
@@ -0,0 +1,10 @@
+
+public class grupo {
+		
+		public String grupoA;
+		public String grupoB;
+		public String grupoC;
+		public String grupoD;
+		
+		public int valor;
+}
---
+++ Aula0504/src/programa.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class programa {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File arquivo = new File ("C:\\Users\\09995133970\\Downloads\\grupos-tabulados\\grupos-tabulados.txt");
+		Scanner leitor = new Scanner(arquivo);
+		
+		int grupoA = 0;
+		
+		
+		while(leitor.hasNext()) {
+			String linha = leitor.nextLine();
+						
+//			System.out.println(linha);
+			
+			String[] dadosLinha = linha.split("\t");
+//			System.out.println("Grupo: " + dadosLinha[0]);
+//			System.out.println("Valor: " + dadosLinha[1]);
+//			System.out.println("-----------------");
+			
+			//calcular o total por grupo;
+			//calcular o total geral;
+			
+			if (dadosLinha[0].equals("A")) {
+				System.out.println("A");
+				}
+			if (dadosLinha[0].equals("A")) {
+				//System.out.println(dadosLinha[1]);
+				int valor = Integer.parseInt(dadosLinha[1]);
+				grupoA =  grupoA + valor;
+				//System.out.println(grupoA);
+			}
+			
+		}
+		System.out.println("Valor total do grupo A: " + grupoA);
+		leitor.close();
+	}
+}
+	
+		
+			
+				
+			
+				
+				
+//				grupo[] grupos = new grupo[5];
+//				int contator = 0 ;
+//				
+//				while(leitor.hasNext()) {
+//					String linha = leitor.nextLine();
+//					String[] dados = linha.split(";");
+//					String grupoA = dados [0];
+//					String grupoB = dados [1];
+//					String grupoC = dados [2];
+//					String grupoD = dados [3];
+//					String grupoE = dados [4];
+//					String grupoF = dados [5];
+				
+
